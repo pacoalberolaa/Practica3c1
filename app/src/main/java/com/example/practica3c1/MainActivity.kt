ca0f48fa2d5c35434b8471b7f7a64626438f0bec
Pacoalberola <falberola2003@gmail.com>
2022-12-04 21:01:21 +0100

    Commit implementacion del xml menu

--- app/src/main/java/com/example/practica3c1/MainActivity.kt
+++ app/src/main/java/com/example/practica3c1/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.practica3c1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+   
 }
