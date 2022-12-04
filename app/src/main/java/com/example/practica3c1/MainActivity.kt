7dc8f908cf206c8024b36b8b91c4821cfa11be78
Pacoalberola <falberola2003@gmail.com>
2022-12-04 21:01:56 +0100

    Commit codigo para el menu

--- app/src/main/java/com/example/practica3c1/MainActivity.kt
+++ app/src/main/java/com/example/practica3c1/MainActivity.kt
@@ -11,5 +11,40 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-   
+    /**
+     * Añadimos el activity_menu al MainActivity
+     */
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.activity_menu,menu)
+        return true
+    }
+
+    /**
+     * Funciones del menu
+     */
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+            R.id.add -> {
+                Toast.makeText(this,"add", Toast.LENGTH_LONG).show()
+                true
+            }
+            R.id.delete ->{
+                Toast.makeText(this,"delete", Toast.LENGTH_LONG).show()
+                true
+            }
+            R.id.action_settings ->{
+                Toast.makeText(this,"action setting", Toast.LENGTH_LONG).show()
+                true
+            }
+            R.id.search ->{
+                Toast.makeText(this,"search", Toast.LENGTH_LONG).show()
+                true
+            }
+            R.id.edit -> {
+                Toast.makeText(this, "edit", Toast.LENGTH_LONG).show()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 }
